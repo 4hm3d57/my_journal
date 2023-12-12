48c05e9388c0280037c62601343a75c5e680dbb7
4хмед57 <4hm3d57@gmail.com>
2023-12-13 01:54:58 +0300

    .

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require('express');
-const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
 
+const app = express();
 const PORT = 3000;
 
+app.use(express.static(path.join(__dirname, 'src')));
+app.use(bodyParser.urlencoded({ extended: false}));
+
+//routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+})
+
+app.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src', 'signup.html'));
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src', 'login.html'));
+})
+
+app.get('/journal', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src', 'journal.html'));
+})
 
 
-app.listen(PORT, () => {
+app.listen(PORT, (req, res) => {
     console.log(`server is running in http://localhost:${PORT}`);
 })
