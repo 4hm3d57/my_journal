61c40ff5afeed387ad311555325a37de0c5b051d
4хмед57 <4hm3d57@gmail.com>
2024-01-15 18:08:57 +0300

    added install.bat

---
+++ install.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Installing Node.js dependencies...
+
+REM Change the path to your project directory
+cd "C:\Users\Ahmed\programming\my_journal"
+
+REM Install Express and other dependencies
+npm install express path mongoose body-parser bcrypt
+
+echo Node.js dependencies installed successfully.
+pause
+
